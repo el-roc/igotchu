0b3d1a3ef64a3733bacfc62abd353b7f814b2d51
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-22 12:55:27 -0400

    editing server.js

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 ////
-app.use(express.static(path.join(__dirname, 'react-ui/build')));
+app.use(express.static(path.join(__dirname, 'create-react-app/build')));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-// connect express_backend 
+// connect express_backend
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
 ////////
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/react-ui/build/index.html'));
+  res.sendFile(path.join(__dirname+'/create-react-app/build/index.html'));
 });
